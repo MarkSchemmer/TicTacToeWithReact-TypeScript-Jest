2ecdf406ad2694696e4430721f8d987f82dc6e11
Ze-Marcos <gringojaimes20@gmail.com>
2019-01-29 12:13:19 -0800

    updated component to indicate whether there was a winner/loser/tie game

--- tictactoe/src/components/Board/Board.tsx
+++ tictactoe/src/components/Board/Board.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Square from '../Square/Square'
 import './Board.css'
 
+import * as api from '../../API/api'
+
 
 interface IProps {
 
@@ -10,7 +12,8 @@ interface IProps {
 interface IState {
     name : string, 
     board : any,
-    turn : number 
+    turn : number,
+    IsWinner : number | null 
 }
 
 class Board extends Component<IProps, IState> {
@@ -19,7 +22,8 @@ class Board extends Component<IProps, IState> {
     public state : IState = {
         name : 'Mark',
         board : genBoard(),
-        turn : 0
+        turn : 0,
+        IsWinner : null 
     }
 
 
@@ -32,20 +36,74 @@ class Board extends Component<IProps, IState> {
             copy.board[x][y].val = 'X'
         else 
             copy.board[x][y].val = 'O'
-        this.setState(copy)
+        this.setState(copy, () => {
+            this.checkForWinner()
+        })
     }
 
+    private SetIsWinner = (option:number|null) => this.setState({ IsWinner : option })  
 
+    public checkForWinner = () => {
+        const hasWinner = api.IsWinner(this.state.board)
+        let whichOption = null 
+
+        console.log(hasWinner)
+
+        if(hasWinner===api.OPTIONS.XWIN)
+            whichOption = api.OPTIONS.XWIN
+        else if (hasWinner === api.OPTIONS.XLOSE) 
+              whichOption = api.OPTIONS.XLOSE
+        else if (hasWinner === api.OPTIONS.TIE)
+                whichOption = api.OPTIONS.TIE
+
+     if (whichOption)
+        this.setState( { IsWinner : whichOption }, () => {
+            this.setWinner()
+        } )
+        
+    }
+
+    public setWinner = () => {
+        if(this.state.IsWinner !== null){
+
+            if(this.state.IsWinner===api.OPTIONS.TIE){
+                return (
+                    <React.Fragment>
+                        TIE GAME!
+                    </React.Fragment>
+                )
+            } else {
+                if(this.state.IsWinner===api.OPTIONS.XWIN){
+                    return (
+                        <React.Fragment>
+                            Player X You Win!
+                        </React.Fragment>
+                    )
+                } else {
+                   return ( <React.Fragment>
+                        Player O You Win!
+                   </React.Fragment> )
+                }
+            }
+
+        } else { 
+            return (
+                <React.Fragment>
+                    Player { this.state.turn % 2 === 0 ? <strong>{'X'}</strong> : <strong>{'O'}</strong> } your move!
+                </React.Fragment>
+            )
+        }
+    }
 
     public render () {
         return (
             <React.Fragment>
                 <div className="main">
                   <div className="greetings">Greetings { this.state.name }</div>
-                  <div className="whoms-turn"> Player { this.state.turn % 2 === 0 ? <strong>{'X'}</strong> : <strong>{'O'}</strong> } your move! </div>
+                  <div className="whoms-turn">  { this.setWinner() } </div>
                     <div className="board">
                             { this.state.board.map((x:any) => 
-                               <div className="row" key={Date.now()*(Math.random()*100)}>
+                               <div className="row" key={ Date.now() * (Math.random() * 100) }>
                                     { x.map((z:any) => <Square key={z.id} {...z} 
                                         onClick={this.onClick.bind(this)} /> )}
                                </div> )}
